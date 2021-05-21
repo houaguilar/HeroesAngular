5c7867f45aa03ac47625b1e59edb9d37efd99c8c
Jordy Aguilar <55517744+houaguilar@users.noreply.github.com>
2021-05-21 07:16:22 -0500

    Add tutorial Tour of Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Endeavor' },
+  { id: 12, name: 'Hawks' },
+  { id: 13, name: 'Best Jeanist' },
+  { id: 14, name: 'Edgeshot' },
+  { id: 15, name: 'Mirko' },
+  { id: 16, name: 'Crust' },
+  { id: 17, name: 'Kamui Woods' },
+  { id: 18, name: 'Wash' },
+  { id: 19, name: 'Yoroi Musha' },
+  { id: 20, name: 'Ryukyu' }
+];
